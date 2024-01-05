dd43fbd17d5a74285c6f489ee21b1934d5f25d59
Gerogiannis <54914456+GEROGIANNIS@users.noreply.github.com>
2024-01-05 16:59:48 +0200

    Create 1_Mexri_9.ino
    Arxikopoihsh ton metablitwn

---
+++ 1_Mexri_9.ino
@@ -0,0 +1,23 @@
+int pinA = 13;
+int pinB = 12;
+int pinC = 11;
+int pinD = 10;
+int pinE = 9;
+int pinF = 8;
+int pinG = 7;
+
+int t = 1000;
+
+void setup() {
+  pinMode(pinA, OUTPUT);
+  pinMode(pinB, OUTPUT);
+  pinMode(pinC, OUTPUT);
+  pinMode(pinD, OUTPUT);
+  pinMode(pinE, OUTPUT);
+  pinMode(pinF, OUTPUT);
+  pinMode(pinG, OUTPUT);
+}
+
+void loop() {
+  
+}
