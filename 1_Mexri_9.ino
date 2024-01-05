50ccb929366ede679ad38e5fbcc72e7f6f35cb76
Gerogiannis <54914456+GEROGIANNIS@users.noreply.github.com>
2024-01-05 17:01:51 +0200

    Update 1_Mexri_9.ino
    Final commit

--- 1_Mexri_9.ino
+++ 1_Mexri_9.ino
@@ -19,5 +19,103 @@ void setup() {
 }
 
 void loop() {
-  
+  //https://i.stack.imgur.com/yerTi.png pinakas alitheias tis othonis 7 segment
+  digitalWrite(pinA , 1);
+  digitalWrite(pinB , 1);
+  digitalWrite(pinC , 1);
+  digitalWrite(pinD , 1);
+  digitalWrite(pinE , 1);
+  digitalWrite(pinF , 1);
+  digitalWrite(pinG , 0);
+  delay(t);
+
+  // 1
+  digitalWrite(pinA , 0);
+  digitalWrite(pinB , 1);
+  digitalWrite(pinC , 1);
+  digitalWrite(pinD , 0);
+  digitalWrite(pinE , 0);
+  digitalWrite(pinF , 0);
+  digitalWrite(pinG , 0);
+  delay(t);
+
+  // 2
+  digitalWrite(pinA , 1);
+  digitalWrite(pinB , 1);
+  digitalWrite(pinC , 0);
+  digitalWrite(pinD , 1);
+  digitalWrite(pinE , 1);
+  digitalWrite(pinF , 0);
+  digitalWrite(pinG , 1);
+  delay(t);
+
+  // 3
+  digitalWrite(pinA , 1);
+  digitalWrite(pinB , 1);
+  digitalWrite(pinC , 1);
+  digitalWrite(pinD , 1);
+  digitalWrite(pinE , 0);
+  digitalWrite(pinF , 0);
+  digitalWrite(pinG , 1);
+  delay(t);
+
+  // 4
+  digitalWrite(pinA , 0);
+  digitalWrite(pinB , 1);
+  digitalWrite(pinC , 1);
+  digitalWrite(pinD , 0);
+  digitalWrite(pinE , 0);
+  digitalWrite(pinF , 1);
+  digitalWrite(pinG , 1);
+  delay(t);
+
+  // 5
+  digitalWrite(pinA , 1);
+  digitalWrite(pinB , 0);
+  digitalWrite(pinC , 1);
+  digitalWrite(pinD , 1);
+  digitalWrite(pinE , 0);
+  digitalWrite(pinF , 1);
+  digitalWrite(pinG , 1);
+  delay(t);
+
+  // 6
+  digitalWrite(pinA , 1);
+  digitalWrite(pinB , 0);
+  digitalWrite(pinC , 1);
+  digitalWrite(pinD , 1);
+  digitalWrite(pinE , 1);
+  digitalWrite(pinF , 1);
+  digitalWrite(pinG , 1);
+  delay(t);
+
+  // 7
+  digitalWrite(pinA , 1);
+  digitalWrite(pinB , 1);
+  digitalWrite(pinC , 1);
+  digitalWrite(pinD , 0);
+  digitalWrite(pinE , 0);
+  digitalWrite(pinF , 0);
+  digitalWrite(pinG , 0);
+  delay(t);
+
+  // 8
+  digitalWrite(pinA , 1);
+  digitalWrite(pinB , 1);
+  digitalWrite(pinC , 1);
+  digitalWrite(pinD , 1);
+  digitalWrite(pinE , 1);
+  digitalWrite(pinF , 1);
+  digitalWrite(pinG , 1);
+  delay(t);
+
+  // 9
+  digitalWrite(pinA , 1);
+  digitalWrite(pinB , 1);
+  digitalWrite(pinC , 1);
+  digitalWrite(pinD , 1);
+  digitalWrite(pinE , 0);
+  digitalWrite(pinF , 1);
+  digitalWrite(pinG , 1);
+  delay(t);
 }
